452f358c8a4933ac043715b5c5ff6fa651cd2185
stvcls1408 <stvcls1408@naver.com>
2020-12-21 17:32:06 +0900

    create layout

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser())
 
 app.get('/', (req, res) => {
   var context = {};
   res.render('index', context);
+})
+
+app.post('/api/v1/regist', (req, res) => {
+  var inputId = req.body.inputId;
+  var inputPw = req.body.inputPw;
+
+  console.log('inputId = ', inputId);
+  console.log('inputPw = ', inputPw);
+
+  // db insert
+  
+  res.json({'result': 'success'});
+  // res.json({'result': 'fail'});
 })
 
 app.listen(port, () => {
