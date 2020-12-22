de81757b3dd0a68f1fe9168d23a03801c7b11152
stvcls1408 <stvcls1408@naver.com>
2020-12-22 15:12:47 +0900

    ss

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+var mysql = require('mysql');
+const { ConnectionError } = require('sequelize');
+
+const models = require('./models');
+const router = express.Router();
+
+// var sequelize = require('./models').sequelize;
+// sequelize.sync();
+
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -12,19 +21,79 @@ app.get('/', (req, res) => {
   res.render('index', context);
 })
 
+// router.post('/api/v1/regist', function(req, res, next) {
+//   var inputName = req.body.inputName;
+//   var inputPw = req.body.inputPw;
+
+// models.post.create({  
+//   username: inputName,
+//   passowrd: inputPw
+// })
+// .then( result => {
+//   console.log("데이터 추가 완료");  
+// })
+// .catch( err => {
+//   console.log("데이터 추가 실패");
+// })
+// });
+
+
+
 app.post('/api/v1/regist', (req, res) => {
-  var inputId = req.body.inputId;
+  var inputName = req.body.inputName;
   var inputPw = req.body.inputPw;
 
-  console.log('inputId = ', inputId);
+  var con = mysql.createConnection({
+    user: "dev",
+    password: "password",
+    host: "192.168.33.80",
+    database: "database_development"
+  });  
+  
+  console.log('inputName = ', inputName);
   console.log('inputPw = ', inputPw);
+
+  res.send(req.body);
 
   // db insert
   
-  res.json({'result': 'success'});
+  // con.connect(function(err) {  
+  //   if (err) throw err;
+  //   console.log("Connected!");
+  //   //var sql = "INSERT INTO users (id, username, password, create_at, update_at) VALUES ? " ;
+  //   models.post.create({
+  //       username: inputName,
+  //       password: inputPw
+  //     });        
+
+  //   con.query(sql, function (err, result) {
+  //     if (err) throw err;
+  //     console.log("1 record inserted");
+  //   });
+  // });
+
+  
+
+
+
+  // ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+  con.connect(function(err) {   // query로 db넣기 성공
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "INSERT INTO users (username, password, create_at, update_at) VALUES ('"+inputName+"','"+inputPw+"',now(),now())";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+    });
+  });
+  // ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+  // res.json({'result': 'success'});
   // res.json({'result': 'fail'});
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+module.exports = router;
